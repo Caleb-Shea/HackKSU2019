82a987cd476ce19ce72262b5226804115ac64ce6
Caleb-Shea <40639492+Caleb-Shea@users.noreply.github.com>
2019-11-09 06:46:48 -0600

    Updates to python files

--- main.py
+++ main.py
@@ -13,17 +13,20 @@ def terminate():
     pyg.quit()
     raise SystemExit()
 
-def get_grass(window, x, y):
-    return BGTile(window, f"grass{random.choice(grass_pallet)}", x, y)
-
-def init_grass():
+def init_grass(level):
     bg_tiles = pyg.sprite.Group()
     for i in range(1, 19):
         for j in range(1, 10):
-            tile = get_grass(window, 64*i, 64*j)
+            tile = BGTile(window, f"grass{random.choice(lvl_data[level]['grass_pallet'])}", 64*i, 64*j)
             bg_tiles.add(tile)
 
     return bg_tiles
+
+def next_level(window, level, player, tiles, pitfalls):
+    level = str(int(level) + 1)
+    player.__init__(window, level)
+    tiles.empty()
+    pitfalls.empty()
 
 
 if __name__ == '__main__':
@@ -34,18 +37,23 @@ if __name__ == '__main__':
 
     clock = pyg.time.Clock()
     score = 0
-    level = 1
-    time = lvl_data['lvl1']['time']
+    level = '1'
+    time = lvl_data[level]['time']
 
 
     font = pyg.font.Font(font_path, 28)
 
-    player = Player(window)
+    player = Player(window, level)
     border = Border(window)
-    bg_tiles = init_grass()
+    bg_tiles = init_grass(level)
     tiles = pyg.sprite.Group()
     tile = Tile(window, 'grass5', 300, 300)
     tiles.add(tile)
+    tile = Tile(window, 'stone', 512, 512)
+    tiles.add(tile)
+    pitfalls = pyg.sprite.Group()
+    pitfall = Pitfall(window, 128, 128)
+    pitfalls.add(pitfall)
 
     while True:
 
@@ -56,27 +64,41 @@ if __name__ == '__main__':
             elif event.type == pyg.KEYDOWN:
                 if event.key == pyg.K_ESCAPE:
                     terminate()
+                elif event.key == pyg.K_SPACE:
+                    next_level(window, level, player, tiles, pitfalls)
+                    level = str(int(level) + 1)
+                    time = lvl_data[level]['time']
+                    bg_tiles = init_grass(level)
             elif event.type == timer_tick:
                 time -= 1
+                if time == 0:
+                    player.die()
+                    time = lvl_data[level]['time']
+
 
 
         event_keys = pyg.key.get_pressed()
         if event_keys[pyg.K_a]:
             if player.vel_x > -player.speed:
                 player.vel_x -= player.accel
+                player.image = pyg.image.load(img_path['player4'])
         if event_keys[pyg.K_d]:
             if player.vel_x < player.speed:
                 player.vel_x += player.accel
+                player.image = pyg.image.load(img_path['player2'])
         if event_keys[pyg.K_w]:
             if player.vel_y > -player.speed:
                 player.vel_y -= player.accel
+                player.image = pyg.image.load(img_path['player1'])
         if event_keys[pyg.K_s]:
             if player.vel_y < player.speed:
                 player.vel_y += player.accel
+                player.image = pyg.image.load(img_path['player3'])
 
         score_text = font.render(f"Score: {score}", True, color['black'])
         level_text = font.render(f"Level: {level}", True, color['black'])
         timer_text = font.render(f"Time Remaining: {time}", True, color['black'])
+        death_text = font.render(f"Deaths: {player.deaths}", True, color['black'])
 
         window.fill(color['bg'])
 
@@ -86,12 +108,16 @@ if __name__ == '__main__':
             tile.render()
         border.render()
 
-        player.update(border.rect, tiles)
+        for pit in pitfalls:
+            pit.render()
+
+        player.update(border.rect, tiles, pitfalls)
         player.render()
 
         window.blit(score_text, (15, 15))
-        window.blit(level_text, (1000, 15))
-        window.blit(timer_text, (500, 15))
+        window.blit(level_text, (1075, 15))
+        window.blit(timer_text, (700, 15))
+        window.blit(death_text, (300, 15))
 
         pyg.display.flip()
 
