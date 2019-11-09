e91fdd7d256c5f74a8e1c4a0a548f49e360af020
Caleb-Shea <40639492+Caleb-Shea@users.noreply.github.com>
2019-11-09 01:43:19 -0600

    Updates to all python files

--- classes.py
+++ classes.py
@@ -8,9 +8,11 @@ class Player(pyg.sprite.Sprite):
         super().__init__()
         self.window = window
         self.image = pyg.image.load(img_path['player'])
+        self.image = pyg.transform.scale(self.image, (32, 32))
         self.rect = self.image.get_rect()
 
-        self.speed = 8
+        self.speed = 16
+        self.accel = 2
 
         self.vel_x = 0
         self.vel_y = 0
@@ -24,9 +26,48 @@ class Player(pyg.sprite.Sprite):
     def move_y(self):
         self.rect.y += self.vel_y
 
-    def update(self):
+    def border_check(self, border):
+        if self.rect.bottom > border.bottom:
+            self.vel_y = 0
+            self.rect.bottom = border.bottom
+
+        if self.rect.top < border.top:
+            self.vel_y = 0
+            self.rect.top = border.top
+
+        if self.rect.left < border.left:
+            self.vel_x = 0
+            self.rect.left = border.left
+
+        if self.rect.right > border.right:
+            self.vel_x = 0
+            self.rect.right = border.right
+
+    def collide_x(self, tiles):
+        for tile in tiles:
+            if pyg.Rect.colliderect(self.rect, tile.rect):
+                if self.vel_x < 0:
+                    self.rect.left = tile.rect.right
+                    self.vel_x = 0
+                elif self.vel_x > 0:
+                    self.rect.right = tile.rect.left
+                    self.vel_x = 0
+
+    def collide_y(self, tiles):
+        for tile in tiles:
+            if pyg.Rect.colliderect(self.rect, tile.rect):
+                if self.vel_y < 0:
+                    self.rect.top = tile.rect.bottom
+                    self.vel_y = 0
+                elif self.vel_y > 0:
+                    self.rect.bottom = tile.rect.top
+                    self.vel_y = 0
+
+    def update(self, border, tiles):
         self.move_x()
+        self.collide_x(tiles)
         self.move_y()
+        self.collide_y(tiles)
 
         self.vel_x *= .8
         if abs(self.vel_x) < .1: self.vel_x = 0
@@ -34,10 +75,24 @@ class Player(pyg.sprite.Sprite):
         self.vel_y *= .8
         if abs(self.vel_y) < .1: self.vel_y = 0
 
+        self.border_check(border)
 
     def render(self):
         self.window.blit(self.image, self.rect)
 
+
+class BGTile(pyg.sprite.Sprite):
+    def __init__(self, window, type, x, y):
+        super().__init__()
+        self.window = window
+        self.image = pyg.image.load(img_path[type])
+        self.rect = self.image.get_rect()
+
+        self.rect.x = x
+        self.rect.y = y
+
+    def render(self):
+        self.window.blit(self.image, self.rect)
 
 class Tile(pyg.sprite.Sprite):
     def __init__(self, window, type, x, y):
@@ -56,8 +111,7 @@ class Border(pyg.sprite.Sprite):
     def __init__(self, window):
         super().__init__()
         self.window = window
-        self.image = pyg.Rect(64, 64, 1088, 512)
-        self.rect = self.image.get_rect()
+        self.rect = pyg.Rect(64, 64, 1152, 576)
 
     def render(self):
-        self.window.blit(self.image, self.rect)
+        pyg.draw.rect(self.window, color['black'], (64, 64, 1152, 576), 5)
--- main.py
+++ main.py
@@ -1,33 +1,51 @@
 import pygame as pyg
-
+import random
 
 from classes import *
 from constants import *
 
 
+ # Custom events
+timer_tick = pyg.USEREVENT + 1
+pyg.time.set_timer(timer_tick, 1000)
+
 def terminate():
     pyg.quit()
     raise SystemExit()
 
+def get_grass(window, x, y):
+    return BGTile(window, f"grass{random.choice(grass_pallet)}", x, y)
 
-def draw_border(window):
-    pyg.draw.rect(window, color['black'], (64, 64, 1088, 512), 5)
+def init_grass():
+    bg_tiles = pyg.sprite.Group()
+    for i in range(1, 19):
+        for j in range(1, 10):
+            tile = get_grass(window, 64*i, 64*j)
+            bg_tiles.add(tile)
+
+    return bg_tiles
+
 
 if __name__ == '__main__':
     pyg.init()
+    pyg.font.init()
     pyg.display.set_caption("Hack KState")
     window = pyg.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 
     clock = pyg.time.Clock()
+    score = 0
+    level = 1
+    time = lvl_data['lvl1']['time']
+
+
+    font = pyg.font.Font(font_path, 28)
 
     player = Player(window)
+    border = Border(window)
+    bg_tiles = init_grass()
     tiles = pyg.sprite.Group()
-
-    for i in range(1, 19):
-        for j in range(1, 10):
-            tile = Tile(window, 'grass', 64*i, 64*j)
-            tiles.add(tile)
-
+    tile = Tile(window, 'grass5', 300, 300)
+    tiles.add(tile)
 
     while True:
 
@@ -38,36 +56,42 @@ if __name__ == '__main__':
             elif event.type == pyg.KEYDOWN:
                 if event.key == pyg.K_ESCAPE:
                     terminate()
+            elif event.type == timer_tick:
+                time -= 1
 
 
         event_keys = pyg.key.get_pressed()
         if event_keys[pyg.K_a]:
-            if player.vel_x > -16:
-                player.vel_x -= 2
+            if player.vel_x > -player.speed:
+                player.vel_x -= player.accel
         if event_keys[pyg.K_d]:
-            if player.vel_x < 16:
-                player.vel_x += 2
+            if player.vel_x < player.speed:
+                player.vel_x += player.accel
         if event_keys[pyg.K_w]:
-            if player.vel_y > -16:
-                player.vel_y -= 2
+            if player.vel_y > -player.speed:
+                player.vel_y -= player.accel
         if event_keys[pyg.K_s]:
-            if player.vel_y < 16:
-                player.vel_y += 2
+            if player.vel_y < player.speed:
+                player.vel_y += player.accel
 
-
-
-
-
+        score_text = font.render(f"Score: {score}", True, color['black'])
+        level_text = font.render(f"Level: {level}", True, color['black'])
+        timer_text = font.render(f"Time Remaining: {time}", True, color['black'])
 
         window.fill(color['bg'])
 
+        for bg_tile in bg_tiles:
+            bg_tile.render()
         for tile in tiles:
             tile.render()
-        draw_border(window)
+        border.render()
 
-        player.update()
+        player.update(border.rect, tiles)
         player.render()
 
+        window.blit(score_text, (15, 15))
+        window.blit(level_text, (1000, 15))
+        window.blit(timer_text, (500, 15))
 
         pyg.display.flip()
 
