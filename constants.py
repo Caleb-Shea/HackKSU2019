9d62e90568de0e7a8955555755a54ef51a8f97fb
Caleb-Shea <40639492+Caleb-Shea@users.noreply.github.com>
2019-11-08 23:26:45 -0600

    Add files via upload

--- constants.py
+++ constants.py
@@ -1,6 +1,8 @@
-WIN_WIDTH = 1200
-WIN_HEIGHT = 700
+WIN_WIDTH = 1280
+WIN_HEIGHT = 704
 
-color = {'bg': (200, 200, 250)}
+color = {'bg': (200, 200, 250),
+         'black': (0, 0, 0)}
 
-img_path = {'grass':'./assets/imgs/grass.png'}
+img_path = {'player':'/Users/21sheac/Desktop/Coding/HackKState/assets/imgs/Grass-1.png',
+            'grass':'/Users/21sheac/Desktop/Coding/HackKState/assets/imgs/Grass-3.png'}
